5b6702052c8e552bacc6eaeff58a6dc86c715751
Jakeobie <36485981+Jakeobie@users.noreply.github.com>
2018-06-23 15:40:09 -0700

    Update ATMServer.java
    Finished Cases 0, 1, and 4. Use method "requestResponse" to send back messages to the client.

--- ATMServer.java
+++ ATMServer.java
@@ -49,8 +49,7 @@ public class ATMServer {
 
                 switch (requestType) {
 
-                    case 0:
-                        //Login
+                    case 0: //Login
                         int count = ClientDatabase.accountList.size();
                         Boolean foundAccount = false;
                         for (int i = 0; i <= count; i++) {
@@ -58,89 +57,91 @@ public class ATMServer {
                                 if (ClientDatabase.accountList.get(i).getPin() == atmMessage.getPin()) {
                                     openSessions.put(msg.getAddress(), atmMessage.getAccountNumber());
                                     requestResponse(5, -1, msg.getAddress());
-                                    System.out.println("Login Successful");
+                                    System.out.println(msg.getAddress() + " has successfully signed in with account #" + atmMessage.getAccountNumber() + ".");
                                     foundAccount = true;
                                     break;
                                 } else {
                                     requestResponse(6, -1, msg.getAddress());
-                                    System.out.println("Wrong PIN");
-                                   
+                                    System.out.println("Wrong PIN for account #" + atmMessage.getAccountNumber() + ".");
+
                                 }
-                            } 
+                            }
                         }
                         if (foundAccount == false) {
-                            System.out.println("Account Doesn't Exist");
+                            requestResponse(6, -1, msg.getAddress());
+                            System.out.println("The account number #" + atmMessage.getAccountNumber() + " does not exist!");
                         }
                         break;
 
-                    case 1:
-                        //Balance
+                    case 1: //Balance
                         if (value != null) {
                             for (Account account : ClientDatabase.accountList) {
                                 if (account.getAccountnum() == value) {
+                                    requestResponse(5, account.getBalance(), msg.getAddress());
                                     System.out.println(account.getBalance());
                                 }
                             }
-                        } 
-                        else {
-                            System.out.println("Not Logged In!");
-                        }
-                            System.out.println("Balance");
-
-                        
-                        case 2:
-                        if (value != null) {
-
                         } else {
+                            requestResponse(6, -1, msg.getAddress());
                             System.out.println("Not Logged In!");
                         }
-                        System.out.println("Withdrawl");
 
-                    case 3:
+                    case 2: //Withdrawl
                         if (value != null) {
-
+                            //INSERT CODE FOR WITHDRAWL HERE
                         } else {
+                            requestResponse(6, -1, msg.getAddress());
                             System.out.println("Not Logged In!");
                         }
-                        System.out.println("Deposit");
 
-                    case 4:
+                    case 3: //Deposit
                         if (value != null) {
-
+                            //INSERT CODE FOR DEPOSIT HERE.
                         } else {
+                            requestResponse(6, -1, msg.getAddress());
                             System.out.println("Not Logged In!");
                         }
-                        System.out.println("Logout");
+
+                    case 4: //Logout
+                        if (value != null) {
+                            openSessions.remove(msg.getAddress());
+                            requestResponse(5, -1, msg.getAddress());
+                            System.out.println("Account #" + atmMessage.getAccountNumber() + "(" + msg.getAddress() + ") has logged out of their session!");
+                        } else {
+                            requestResponse(6, -1, msg.getAddress());
+                            System.out.println("Not Logged In!");
+                        }
 
                     default:
+                        requestResponse(6, -1, msg.getAddress());
                         System.out.println("Error");
                 }
 
                 String str = new String(msg.getData(), 0, msg.getLength());
-                        System.out.print(str);
+                System.out.print(str);
 
-                }catch (SocketTimeoutException ste) {    // receive() timed out
+            } catch (SocketTimeoutException ste) {    // receive() timed out
                 System.err.println("Response timed out!");
-            }catch (Exception ioe) {                // should never happen!
+            } catch (Exception ioe) {                // should never happen!
                 System.err.println("Bad receive");
                 ioe.printStackTrace();
             }
 
-                // newline must be part of str
-            }
-            // s.close();
-        } // end of main
-    
-        static public void requestResponse(int responseType, int responseAmount, InetAddress clientAddress) throws IOException, ClassNotFoundException {
-            DatagramSocket Socket;
-            Socket = new DatagramSocket();
-            Client response = new Client(responseType, -1, -1, responseAmount);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); // creates new byte output stream
-            ObjectOutputStream os = new ObjectOutputStream(outputStream);          // creates new out stream
-            os.writeObject(response); // writes new client to send message
-            byte[] b = outputStream.toByteArray(); // writes bytes to array
-            DatagramPacket msg = new DatagramPacket(b, b.length, clientAddress, 4445); // creates new datagram to send with coordinates
-            Socket.send(msg); // sends message
-            
+            // newline must be part of str
         }
+        // s.close();
+    } // end of main
+
+    static public void requestResponse(int responseType, int responseAmount, InetAddress clientAddress) throws IOException, ClassNotFoundException {
+        DatagramSocket Socket;
+        Socket = new DatagramSocket();
+        Client response = new Client(responseType, -1, -1, responseAmount);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); // creates new byte output stream
+        ObjectOutputStream os = new ObjectOutputStream(outputStream);          // creates new out stream
+        os.writeObject(response); // writes new client to send message
+        byte[] b = outputStream.toByteArray(); // writes bytes to array
+        DatagramPacket msg = new DatagramPacket(b, b.length, clientAddress, 4445); // creates new datagram to send with coordinates
+        Socket.send(msg); // sends message
+
     }
+}
