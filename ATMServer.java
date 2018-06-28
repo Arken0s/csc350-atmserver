d1e1ceb4caba64d1dd54d5a669177584daa7cc1c
kevinscaringi <40499841+kevinscaringi@users.noreply.github.com>
2018-06-28 10:25:43 -0700

    Update ATMServer.java
    Deposit and withdraw messages updated. I may not have done the withdraw and deposit gets properly for the amount, but let me know

--- ATMServer.java
+++ ATMServer.java
@@ -88,16 +88,33 @@ public class ATMServer {
 
                     case 2: //Withdrawl
                         if (value != null) {
-                            //INSERT CODE FOR WITHDRAWL HERE
+                            if (account.getAccountnum() == value) {
+                                if (account.getBalance() > atmMessage.getAmount()) {
+                                    account.setBalance((account.getBalance() - atmMessage.getAmount()));
+                                    requestResponse(5, account.getBalance(), msg.getAddress());
+                                    System.out.println(account.getBalance());
+                                }
+                                else {
+                                    requestResponse(6, -1, msg.getAddress());
+                                }
+                            }
                         } else {
                             requestResponse(6, -1, msg.getAddress());
                             System.out.println("Not Logged In!");
                         }
-
                     case 3: //Deposit
                         if (value != null) {
-                            //INSERT CODE FOR DEPOSIT HERE.
-                        } else {
+                            if (account.getAccountnum() == value) {
+                                account.setBalance((account.getBalance() + atmMessage.getAmount()));
+                                requestResponse(5, account.getBalance(), msg.getAddress());
+                                System.out.println(account.getBalance());
+                               
+                                else {
+                                    requestResponse(6, -1, msg.getAddress());
+                                }
+                            }
+                        } 
+                        else {
                             requestResponse(6, -1, msg.getAddress());
                             System.out.println("Not Logged In!");
                         }
